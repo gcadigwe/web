4b176152d7101f4ad6189f5da389a96eec374a9a
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-25 23:45:48 +0100

    fetch function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newname' });
+// user.set({ name: 'newname' });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change event');
+});
+
+user.trigger('change');
+
+// console.log(user.get('name'));
+// console.log(user.get('age'));
