05f77c7409bf698d18d8437df90839fbf3eb143e
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-25 23:06:33 +0100

    trigger

--- src/models/User.ts
+++ src/models/User.ts
@@ -23,4 +23,16 @@ export class User {
 
     this.events[eventName] = handlers;
   }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
 }
