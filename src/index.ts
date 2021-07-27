dde5c0ac6ce1ce6063da0c0b9ebcb1f39b622d03
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-27 15:36:39 +0100

    got attributes working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({});
-
-user.set({ name: 'newname', age: 20 });
-
-// user.fetch();
-user.save();
-
 // console.log(user.get('name'));
 // console.log(user.get('age'));
