b198a5d9d9bf2cf233dadea4c2295e13a3036b5b
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-28 16:21:51 +0100

    fetch coordination implementation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { User } from './models/User';
 
 // console.log(user.get('name'));
 // console.log(user.get('age'));
-const user = new User({ name: 'hwyya', age: 0 });
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-  console.log('change happened');
+  console.log(user);
 });
 
-user.set({ name: 'hawa' });
+user.fetch();
