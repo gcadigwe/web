c75ba86fcf299cad0ba63d397a727f398e59f1a6
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-28 15:46:33 +0100

    coordination

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,8 @@
 export class Attribute<T> {
   constructor(private data: T) {}
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     Object.assign(this.data, update);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './sync';
+import { Attribute } from './Attributes';
 
 export interface UserProps {
   name?: string;
@@ -12,4 +13,21 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attribute: Attribute<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attribute = new Attribute<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attribute.get;
+  }
 }
