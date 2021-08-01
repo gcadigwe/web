00281e5be20b06483824f5bc56ff391d78470053
Sarzy <adigwegodswillchinonso@gmail.com>
2021-08-01 20:27:54 +0100

    abstract class refactoring

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,7 @@
 import { User } from '../models/User';
+import { View } from './View';
 
-export class UserForm {
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-name': this.onSetNameClick,
