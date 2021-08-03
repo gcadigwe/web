250b27a9e206d1149ee068b15a112f912554a428
Sarzy <adigwegodswillchinonso@gmail.com>
2021-08-03 22:30:25 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { UserForm } from './views/UserForm';
-import { User } from './models/User';
+// import { UserForm } from './views/UserForm';
+// import { User } from './models/User';
 
-const user = User.buildUser({ id: 1, name: 'name', age: 10 });
+// const user = User.buildUser({ id: 1, name: 'name', age: 10 });
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-if (root) {
-  const userform = new UserForm(root, user);
-  userform.render();
-} else {
-  throw new Error('Error');
-}
+// if (root) {
+//   const userform = new UserForm(root, user);
+//   userform.render();
+// } else {
+//   throw new Error('Error');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
